6a308812afa1e86b22fea7d09dc4401424dfe813
sungmini.kim <clever1501@naver.com>
2025-07-01 15:25:57 +0900

    Get Gop code add

--- day07_Calculation/main.cpp
+++ day07_Calculation/main.cpp
@@ -2,12 +2,23 @@
 
 class Cal {
 public:
-
+	int getGop(int a, int b) {
+		return a * b;
+	}
 };
 
 //테스트 케이스 작성
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(CalTest, GetGopTest) {
+	Cal cal;
+	EXPECT_EQ(cal.getGop(2, 3), 6);
+	EXPECT_EQ(cal.getGop(0, 5), 0);
+	EXPECT_EQ(cal.getGop(-1, 5), -5);
+	EXPECT_EQ(cal.getGop(4, 0), 0);
+	EXPECT_EQ(cal.getGop(-2, -3), 6);
 }
 
 int main() {

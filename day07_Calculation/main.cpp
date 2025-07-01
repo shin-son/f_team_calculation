48bc6203ff746177de2732af26239bb7360ab352
Hyungju Song <hyungju.song54@gmail.com>
2025-07-01 15:46:27 +0900

    Add Zegops function

--- day07_Calculation/main.cpp
+++ day07_Calculation/main.cpp
@@ -2,7 +2,7 @@
 
 class Cal {
 public:
-
+  int getZegop(int a) { return a * a; };
 };
 
 //테스트 케이스 작성
@@ -10,6 +10,18 @@ TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
 }
 
+TEST(t1, tc_zegop) {
+  Cal cal;
+  EXPECT_EQ(1, cal.getZegop(1));
+  EXPECT_EQ(4, cal.getZegop(2));
+  EXPECT_EQ(16, cal.getZegop(4));
+  EXPECT_EQ(100, cal.getZegop(10));
+  EXPECT_EQ(1, cal.getZegop(-1));
+  EXPECT_EQ(9, cal.getZegop(-3));
+  EXPECT_EQ(144, cal.getZegop(-12));
+  EXPECT_EQ(10000, cal.getZegop(-100));
+
+}
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();

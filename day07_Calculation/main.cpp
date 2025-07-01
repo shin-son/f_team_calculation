90dc41f737bea0b562f86d08e78beeacdd33c3d7
shin.son <thstls110534@naver.com>
2025-07-01 15:27:09 +0900

    minus_operation_commit

--- day07_Calculation/main.cpp
+++ day07_Calculation/main.cpp
@@ -2,12 +2,19 @@
 
 class Cal {
 public:
-
+	int getMinus(int a, int b) {
+		return a - b;
+	}
 };
 
 //테스트 케이스 작성
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, t4) {
+	Cal cal;
+	EXPECT_EQ(cal.getMinus(4, 1), 3);
 }
 
 int main() {

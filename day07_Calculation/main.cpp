e25f76bcec15a7a88a055613f819f10562f3cc08
jw.song <chokosal@naver.com>
2025-07-01 15:30:00 +0900

    add getSumSum

--- day07_Calculation/main.cpp
+++ day07_Calculation/main.cpp
@@ -2,12 +2,21 @@
 
 class Cal {
 public:
-
+	//이곳에 코드 작성
+	int getSumSum(int a, int b, int c)
+	{
+		return a + b + c;
+	}
 };
 
 //테스트 케이스 작성
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, SumSum1) {
+	Cal cal;
+	EXPECT_EQ(6, cal.getSumSum(1, 2, 3));
 }
 
 int main() {

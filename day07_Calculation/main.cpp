b8b680662ebe144f8a28ae79910f796b3b457f95
unknown <jwon96.park@gmail.com>
2025-07-01 15:30:30 +0900

    Add getSum function and TEST code

--- day07_Calculation/main.cpp
+++ day07_Calculation/main.cpp
@@ -2,12 +2,19 @@
 
 class Cal {
 public:
-
+	int getSum(int a, int b) {
+		return a + b;
+	}
 };
 
 //테스트 케이스 작성
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(TS, tc1) {
+	Cal cal;
+	EXPECT_EQ(3, cal.getSum(1, 2));
 }
 
 int main() {
